e0e4b87c3c3c64c568128535f31a26dd33d0c6ca
François Dursus <durss@free.fr>
2013-12-03 00:05:34 +0000

    Fixing huge bug on editor's loading, some events were duplicated depending on the complexity due to the rendering by chunks over time.

--- deploy/editor.php
+++ deploy/editor.php
@@ -59,7 +59,7 @@
 			if(lang != "fr" && lang != "en") lang = "en";
 			
 			var flashvars = {};
-			flashvars["version"] = "70";
+			flashvars["version"] = "71";
 			flashvars["configXml"] = "./xml/config.xml?v="+flashvars["version"];
 			flashvars["lang"] = lang;
 			var attributes = {};
