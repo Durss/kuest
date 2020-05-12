11a741fe98a9ac3c151d8e907423e58a33dfa38f
Francois <durss@free.fr>
2020-05-12 21:44:58 +0200

    adding missing file, removing useless one

---
+++ deploy/php/db/DBConnection.php
@@ -0,0 +1,43 @@
+<?php
+	class DBConnection {
+		private static $connection;
+		private static $SESSION_VERSION = 9;
+		
+		public static function connect() {
+			if ($_SERVER['HTTP_HOST'] != "fevermap.org") {
+				self::$connection = new PDO('mysql:host=localhost;dbname=kuest', 'root', '');
+			}else {
+				self::$connection = new PDO('mysql:host=xxx;dbname=xxx', 'xxx', 'xxx');
+			}
+			
+			if (get_magic_quotes_gpc()) {
+				function stripslashes_gpc(&$value)
+				{
+					$value = stripslashes($value);
+				}
+				array_walk_recursive($_GET, 'stripslashes_gpc');
+				array_walk_recursive($_POST, 'stripslashes_gpc');
+				array_walk_recursive($_COOKIE, 'stripslashes_gpc');
+				array_walk_recursive($_REQUEST, 'stripslashes_gpc');
+			}
+			
+			self::initSession();
+		}
+		
+		public static function initSession() {
+			session_start();
+			if (!isset($_SESSION['kuest_version']) || $_SESSION['kuest_version'] != self::$SESSION_VERSION) {
+				unset($_SESSION['kuest_logged']);
+				$_SESSION['kuest_version'] = self::$SESSION_VERSION;
+			}
+		}
+		
+		public static function close() {
+			self::$connection = null;
+		}
+		
+		public static function getLink() {
+			return self::$connection;
+		}
+	}
+?>
